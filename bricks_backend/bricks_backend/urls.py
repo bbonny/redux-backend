e62111cdfc926a448588299ca5c83628385deac4
Benjamin Bonny <benjaminbonny@Benjamins-MacBook-Pro.local>
2016-03-26 19:58:14 +0100

    Add simple brick route
    Author:    Benjamin Bonny <benjamin.bonny@gmail.com>

--- bricks_backend/bricks_backend/urls.py
+++ bricks_backend/bricks_backend/urls.py
@@ -3,7 +3,10 @@ from django.contrib import admin
 
 from django.conf.urls import url, include
 from django.contrib.auth.models import User
+
 from rest_framework import routers, serializers, viewsets
+
+from bricks.models import Brick
 
 # Serializers define the API representation.
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -16,9 +19,23 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+
+class BrickSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Brick
+        fields = ('name', 'start', 'count', 'remote_path', 'column', 'checked')
+
+class BrickViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Brick.objects.all()
+    serializer_class = BrickSerializer
+
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
+router.register(r'brick', BrickViewSet)
 
 urlpatterns = [
     # Examples:
